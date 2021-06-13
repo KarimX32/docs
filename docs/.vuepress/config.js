e4749407450b449fbf8e22461318b02ed733c19b
jac <48368615+JacProsser@users.noreply.github.com>
2021-06-13 22:43:02 +0100

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 const { description, name } = require('../../package')
-const sidebar = require("./components/sidebar.js")
+// const sidebar = require("./components/sidebar.js")
 
 
 
@@ -38,9 +38,54 @@ module.exports = {
         link: 'https://github.com/AngeloCore/discord-buttons'
       },
     ],
-    sidebar
-  },
-
+    sidebar {
+    '/documentation/': [
+    {
+      title: 'Getting Started',
+      collapsable: false,
+      children: [
+        './getting-started/installation',
+        './getting-started/setup',
+      ]
+    },
+    {
+      title: 'Sending Buttons',
+      collapsable: false,
+      children: [
+        './sending-buttons/channel-send',
+        './sending-buttons/message-edit',
+        './sending-buttons/message-reply',
+        './sending-buttons/remove-buttons'
+      ]
+    },
+    {
+      title: 'Contructors',
+      collapsable: false,
+      children: [
+        './constructors/message-button',
+        './constructors/message-action-row',
+      ]
+    },
+    {
+      title: 'Events/Collectors',
+      collapsable: false,
+      children: [
+        './events/clickButton',
+        './events/createButtonCollector',
+        './events/awaitButtons',
+      ]
+    },
+    {
+      title: 'Miscellaneous',
+      collapsable: false,
+      children: [
+        './miscellaneous/examples',
+        './miscellaneous/faq',
+      ]
+    },
+  ]
+ },
+},
 
   markdown: {
     lineNumbers: true,
